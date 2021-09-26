df5d2b173bcfde10438e9ef37e8c47d56a9e5a30
Jonathan Lucas de Matos Diniz <jonathan.lucas.m@gmail.com>
2021-09-26 17:23:39 -0300

    Update login

--- src/functions/sessions/login.js
+++ src/functions/sessions/login.js
@@ -1,9 +1,10 @@
-import { User } from "../../db/models.js";
+import { User, Pantry } from "../../db/models.js";
 import { compareHash } from "../../providers/hashProvider.js";
 import AppError from "../../errors/AppError.js";
 
 async function login(request, response) {
   const { password, username } = request.body;
+  console.log(password, username);
 
   const user = await User.findOne({
     username,
@@ -19,11 +20,18 @@ async function login(request, response) {
     throw new AppError("Usuário/Senha incorretos.", 401);
   }
 
+  let pantry = null;
+
+  if (!!user?.pantry[0]?.id) {
+    pantry = await Pantry.findById(user.pantry[0].id);
+  }
+
   const { password: _, ...userWithoutPassword } = user._doc;
 
   return response.json({
     id: user._id,
     ...userWithoutPassword,
+    hash: pantry ? pantry._doc.hash : null,
   });
 }
 
